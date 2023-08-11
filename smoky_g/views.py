0928b89ca78b6e50337bf4640bbb0113095a7aad
JarradBaker <jarradbaker@outlook.com>
2023-08-11 22:29:31 +0000

    Fix: blog page works and shows blog posts

--- smoky_g/views.py
+++ smoky_g/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 
 def home(request):
     return render(request, 'index.html')
+
+
+def blog(request):
+    blogs = Blog.objects.all()
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'blog.html', context)
